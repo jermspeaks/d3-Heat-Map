557e9a0854853923e273b52deec31106134bb784
Jeremy Wong <jermspeaks@gmail.com>
2015-03-11 12:04:01 -0700

    created build for js libraries. created base js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,42 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var filter = require('gulp-filter');
+var mainBowerFiles = require('main-bower-files');
 
 var paths = {
   js: 'js/*.js',
   css: 'css/*.css',
   images: 'img/*'
+};
+
+gulp.task('build', ['process']);
+
+gulp.task('process', function() {
+    return processLibraries();
+});
+
+function filterByExtension(extension) {
+    return filter(function(file) {
+        return file.path.match(new RegExp('.' + extension + '$'));
+    });
+};
+
+function processLibraries() {
+  console.log('Processing :: Vendor JS Libraries');
+  var mainFiles = mainBowerFiles({
+    checkExistence: true
+  });
+  var jsFilter = filterByExtension('js');
+
+  if (!mainFiles.length) {
+    // No files found
+    return;
+  }
+
+  return gulp.src(mainFiles)
+    .pipe(jsFilter)
+    // .pipe(uglify())
+    // .pipe(notify('<%= file.relative %>'))
+    .pipe(concat('lib.js'))
+    .pipe(gulp.dest('build/js'));
 };
--- js/d3HeatMap.js
+++ js/d3HeatMap.js
@@ -1,1 +1,5 @@
+$(document).ready(function() {
 
+  // Write code here
+
+});
