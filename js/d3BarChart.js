f3ab070abc74635b1f01ce8628faff98f146b480
Jeremy Wong <jermspeaks@gmail.com>
2015-03-11 15:30:00 -0700

    still learning d3 transitions

--- js/d3BarChart.js
+++ js/d3BarChart.js
@@ -1,26 +1,103 @@
 $(document).ready(function() {
 
   // Attributes
-  var w = 500;
-  var h = 100;
+  var w = 600;
+  var h = 250;
   var barPadding = 1;
+  var padding = 30;
+
+  var svg = d3.select('.container')
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h);
 
   // Data
-  var dataset = [100, 200, 300, 400, 500];
-  // var dataset = [
-  //   [5, 20],
-  //   [480, 90],
-  //   [250, 50],
-  //   [100, 33],
-  //   [330, 95],
-  //   [410, 12],
-  //   [475, 44],
-  //   [25, 67],
-  //   [85, 21],
-  //   [220, 88]
-  // ];
+  var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13, 11, 12, 15, 20, 18, 17, 16, 18, 23, 25];
 
-  // SVG Scatterplot
+  // Scales
+  var xScale = d3.scale.ordinal()
+    .domain(d3.range(dataset.length))
+    .rangeRoundBands([0, w], 0.05);
+
+  var yScale = d3.scale.linear()
+    .domain([0, d3.max(dataset)])
+    .range([0, h]);
+
+  // Axes
+
+  // Create Bars
+  svg.selectAll('rect')
+    .data(dataset)
+    .enter()
+    .append('rect')
+    .attr('x', function(d, i) {
+      return xScale(i);
+    })
+    .attr('y', function(d) {
+      return h - yScale(d);
+    })
+    .attr('width', xScale.rangeBand())
+    .attr('height', function(d) {
+      return yScale(d);
+    })
+    .attr('fill', function(d) {
+      return 'rgb(0, 0, ' + (d * 10) + ')';
+    });
+
+  // Create Bar Labels
+  svg.selectAll("text")
+    .data(dataset)
+    .enter()
+    .append("text")
+    .text(function(d) {
+      return d;
+    })
+    .attr("x", function(d, i) {
+      return xScale(i) + xScale.rangeBand() / 2;
+    })
+    .attr("y", function(d) {
+      return h - yScale(d) + 14;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("fill", "white")
+    .attr("text-anchor", "middle");
+
+  // Data
+  // var dataset = [];
+  // var numDataPoints = 50;
+  // var xRange = Math.random() * 1000;
+  // var yRange = Math.random() * 1000;
+  // for (var i = 0; i < numDataPoints; i++) {
+  //   var newNumber1 = Math.floor(Math.random() * xRange);
+  //   var newNumber2 = Math.floor(Math.random() * yRange);
+  //   dataset.push([newNumber1, newNumber2]);
+  // }
+
+  // Scales
+  // var xScale = d3.scale.linear()
+  //   .domain([0, d3.max(dataset, function(d) { return d[0]; })])
+  //   .range([padding, w - padding * 2]);
+
+  // var yScale = d3.scale.linear()
+  //   .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+  //   .range([h - padding, padding]);
+
+  // var rScale = d3.scale.linear()
+  //   .domain([0, d3.max(dataset, function(d) { return d[1]; })])
+  //   .range([2, 5]);
+
+  // // Axes
+  // var xAxis = d3.svg.axis()
+  //   .scale(xScale)
+  //   .orient('bottom')
+  //   .ticks(5);
+
+  // var yAxis = d3.svg.axis()
+  //   .scale(yScale)
+  //   .orient('left')
+  //   .ticks(5);
+  // // SVG Scatterplot
   // var svg = d3.select('.container')
   //   .append('svg')
   //   .attr('width', w)
@@ -31,13 +108,13 @@ $(document).ready(function() {
   //   .enter()
   //   .append('circle')
   //   .attr('cx', function(d) {
-  //     return d[0];
+  //     return xScale(d[0]);
   //   })
   //   .attr('cy', function(d) {
-  //     return d[1];
+  //     return yScale(d[1]);
   //   })
   //   .attr('r', function(d) {
-  //     return Math.sqrt(h - d[1]);
+  //     return rScale(d[1]);
   //   });
 
   // svg.selectAll('text')
@@ -48,14 +125,24 @@ $(document).ready(function() {
   //     return d[0] + ', ' + d[1];
   //   })
   //   .attr('x', function(d) {
-  //     return d[0];
+  //     return xScale(d[0]);
   //   })
   //   .attr('y', function(d) {
-  //     return d[1];
+  //     return yScale(d[1]);
   //   })
   //   .attr("font-family", "sans-serif")
   //   .attr("font-size", "11px")
   //   .attr("fill", "red");
+
+  // svg.append('g')
+  //   .attr('class', 'axis') // Assign 'axis' class
+  //   .attr('transform', 'translate(0,' + (h - padding) + ')')
+  //   .call(xAxis);
+
+  // svg.append('g')
+  //   .attr('class', 'axis') // Assign 'axis' class
+  //   .attr('transform', 'translate(' + padding + ', 0)')
+  //   .call(yAxis);
 
   // SVG Bar Chart
   // var svg = d3.select(".container")
