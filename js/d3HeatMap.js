4b7fc56960efc6b8528a00c68f2ba7cfd1ef295d
Jeremy Wong <jermspeaks@gmail.com>
2015-03-11 17:59:45 -0700

    refactored chart variables

--- js/d3HeatMap.js
+++ js/d3HeatMap.js
@@ -1,109 +1,199 @@
 $(document).ready(function() {
 
-  // Attributes
+  /* =======================================
+      Attributes
+     ======================================= */
   var w = $(window).width() - 200;
   var h = 200;
-  var padding = 30;
-  var viewPadding = 100;
-  var lineHeight = 8;
+
+  // Padding
   var textPadding = 10;
+  var rightEdgePadding = 40;
 
-  // Data
+  // Text Line Height
+  var lineHeight = 8;
+
+  // Label Value Formatter
+  var valueFormatter = d3.format('.1s');
+
+  // Element Widths
+  var yAxisLabelWidth = 100;
+  var viewButtonWidth = 100;
+
+  // Start Position for Bars
+  var startBars = viewButtonWidth + yAxisLabelWidth;
+
+  // End Position for Y Axis Labels
+  var endYAxisLabels = viewButtonWidth + yAxisLabelWidth - textPadding;
+
+  // Find Start Position for Bar Label
+  function startBarLabelPosition(d) {
+    return viewButtonWidth + yAxisLabelWidth + xScale(d) + textPadding;
+  }
+
+  /* =======================================
+      Data
+     ======================================= */
   var dataset = [934, 6000, 12000, 48000, 64000];
-  var colors = ['186, 38, 43', '212,80,51', '246,142,53', '255,190,55', '35,80,116'];
   var categories = ['10+ Page Views', '5-9 Page Views', '2-4 Page Views', '1 Page View', 'All'];
+  var colors = ['186, 38, 43', '212,80,51', '246,142,53', '255,190,55', '35,80,116'];
 
-  // Create SVG Element
+
+  /* =======================================
+      Scales
+     ======================================= */
+
+  // Max Range for width of bars
+  var maxRange = w - viewButtonWidth - yAxisLabelWidth - rightEdgePadding;
+  var xScale = d3.scale.linear()
+    .domain([0, d3.max(dataset)])
+    .range([10, maxRange]);
+
+  var yScale = d3.scale.ordinal()
+    .domain(d3.range(dataset.length))
+    .rangeRoundBands([0, h], 0.2);
+
+  /* =======================================
+      SVG Element
+     ======================================= */
   var svg = d3.select('.container')
     .append('svg')
     .attr('width', w)
     .attr('height', h)
     .style('border', '1px solid black');
 
-  // Scales
-  var xScale = d3.scale.linear()
-    .domain([0, d3.max(dataset)])
-    .range([0, w - 100 - viewPadding]);
+  // Create Chart
+  createChart();
 
-  var yScale = d3.scale.ordinal()
-    .domain(d3.range(dataset.length))
-    .rangeRoundBands([0, h], 0.2);
+  // On Window Resize
+  window.onresize = function(event) {
+    // Update Width
+    w = $(window).width() - 200;
 
-  // Label Value Formatter
-  var valueFormatter = d3.format('.1s');
+    // Update Max Range
+    maxRange = w - viewButtonWidth - yAxisLabelWidth - rightEdgePadding;
+
+    // Update xScale
+    xScale = d3.scale.linear()
+      .domain([0, d3.max(dataset)])
+      .range([10, maxRange]);
+
+    // Resize SVG
+    svg.attr('width', w);
+
+    // Resize Bars
+    svg.selectAll('rect')
+      .data(dataset)
+      .transition()
+      .delay(function(d, i) {
+        return i / dataset.length * 1000;
+      })
+      .duration(200)
+      .ease('linear')
+      .attr('width', function(d) {
+        return xScale(d);
+      });
+
+    // Resize Bar Labels
+    svg.selectAll(".bar-label")
+      .data(dataset)
+      .transition()
+      .delay(function(d, i) {
+        return i / dataset.length * 1000;
+      })
+      .duration(150)
+      .attr("x", function(d, i) {
+        return startBarLabelPosition(d);
+      });
+  };
+
+  function createChart() {
+    createBars();
+    createBarEvents();
+    createYAxisLabels();
+    createBarLabels();
+  }
 
   // Create Bars
-  svg.selectAll('rect')
-    .data(dataset)
-    .enter()
-    .append('rect')
-    .classed('bar', true)
-    .attr('x', 100 + viewPadding)
-    .attr('y', function(d, i) {
-      return yScale(i);
-    })
-    .attr('width', function(d) {
-      return xScale(d) / 1.1; // Padding to the right
-    })
-    .attr('height', yScale.rangeBand())
-    .attr('fill', function(d, i) {
-      return 'rgb(' + colors[i] + ')';
-    });
+  function createBars() {
+    svg.selectAll('rect')
+      .data(dataset)
+      .enter()
+      .append('rect')
+      .classed('bar', true)
+      .attr('x', 100 + yAxisLabelWidth)
+      .attr('y', function(d, i) {
+        return yScale(i);
+      })
+      .attr('width', function(d) {
+        return xScale(d); // Padding to the right
+      })
+      .attr('height', yScale.rangeBand())
+      .attr('fill', function(d, i) {
+        return 'rgb(' + colors[i] + ')';
+      });
+  }
 
   // Mouse Events
-  svg.selectAll('rect')
-    .on('mouseover', function(d, i) {
-      d3.select(this)
-        .attr('fill', function(d) {
-          var index = dataset.indexOf(d);
-          return 'rgba(' + colors[index] + ', 0.4)';
-        });
-    })
-    .on('mouseout', function(d, i) {
-      d3.select(this)
-        .attr('fill', function(d, i) {
-          var index = dataset.indexOf(d);
-          return 'rgb(' + colors[index] + ')';
-        });
-    });
+  function createBarEvents() {
+    svg.selectAll('rect')
+      .on('mouseover', function(d, i) {
+        d3.select(this)
+          .attr('fill', function(d) {
+            var index = dataset.indexOf(d);
+            return 'rgba(' + colors[index] + ', 0.4)';
+          });
+      })
+      .on('mouseout', function(d, i) {
+        d3.select(this)
+          .attr('fill', function(d, i) {
+            var index = dataset.indexOf(d);
+            return 'rgb(' + colors[index] + ')';
+          });
+      });
+  }
 
   // Create yAxis Labels
-  svg.selectAll(".xAxis-label")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .classed('xAxis-label', true)
-    .text(function(d, i) {
-      return categories[i];
-    })
-    .attr("x", 90 + viewPadding)
-    .attr("y", function(d, i) {
-      return yScale(i) + (lineHeight / 2) + (yScale.rangeBand() / 2);
-    })
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "12px")
-    .attr("fill", "black")
-    .style("text-anchor", "end");
+  function createYAxisLabels() {
+    svg.selectAll(".xAxis-label")
+      .data(dataset)
+      .enter()
+      .append("text")
+      .classed('xAxis-label', true)
+      .text(function(d, i) {
+        return categories[i];
+      })
+      .attr("x", 90 + viewButtonWidth)
+      .attr("y", function(d, i) {
+        return yScale(i) + (lineHeight / 2) + (yScale.rangeBand() / 2);
+      })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "12px")
+      .attr("fill", "black")
+      .style("text-anchor", "end");
+  }
 
   // Create Bar Labels
-  svg.selectAll(".bar-label")
-    .data(dataset)
-    .enter()
-    .append("text")
-    .classed('bar-label', true)
-    .text(function(d, i) {
-      return valueFormatter(d);
-    })
-    .attr("x", function(d, i) {
-      return 100 + viewPadding + xScale(d) / 1.1 + textPadding;
-    })
-    .attr("y", function(d, i) {
-      return yScale(i) + (lineHeight / 2) + (yScale.rangeBand() / 2);
-    })
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "12px")
-    .attr("font-weight", 600)
-    .attr("fill", "black");
+  function createBarLabels() {
+    svg.selectAll(".bar-label")
+      .data(dataset)
+      .enter()
+      .append("text")
+      .classed('bar-label', true)
+      .text(function(d, i) {
+        return valueFormatter(d);
+      })
+      .attr("x", function(d, i) {
+        return startBarLabelPosition(d);
+      })
+      .attr("y", function(d, i) {
+        return yScale(i) + (lineHeight / 2) + (yScale.rangeBand() / 2);
+      })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "12px")
+      .attr("font-weight", 600)
+      .attr("fill", "black");
+  }
 
   // View Buttons
 
